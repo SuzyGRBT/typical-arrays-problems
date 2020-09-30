c1c63b8ad20886aa9bde0555cbcffc45afb75941
SuzyGRBT <lady94@inbox.ru>
2020-10-01 00:19:33 +0300

    feat: solve problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (typeof array !== "undefined" && array.length) {
+    let minValue = Math.min.apply(null, array);
+    return minValue;
+    } else {
+    return 0;
+    };
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+
+  if (typeof array !== "undefined" && array.length) {
+    let maxValue = Math.max.apply(null, array);
+    return maxValue;
+    } else {
+    return 0;
+  };
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (typeof array !== "undefined" && array.length) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+    };
+    return sum / array.length;
+    };
+    return 0;
+};
